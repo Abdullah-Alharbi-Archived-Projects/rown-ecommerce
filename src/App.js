c0023f1fdec30297a475738ac74f9333c60c51ae
W2AlharbiMe <w2.alharbi.me@gmail.com>
2020-02-13 00:07:29 +0300

    created first component & styles.scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Home from "./home.component";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }

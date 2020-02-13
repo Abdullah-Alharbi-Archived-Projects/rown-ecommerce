daef1f587ea18e63fc6cee2bc9d7aa2eeabbcd6c
W2AlharbiMe <w2.alharbi.me@gmail.com>
2020-02-13 23:38:50 +0300

    added sign in and sign up pages, and added link in header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/home/home.page.jsx";
 import Shop from "./pages/shop/shop.page";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.page.jsx";
 import "./App.scss";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/shop" component={Shop} />
+        <Route path="/sign-in" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
